a47e4ae18fedce7fae9008b577a01989e9aa1c3f
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2024-09-24 13:44:06 +0200

    Update AutoConfigureDataSourceListener.java

--- src/main/java/de/php_perfect/intellij/ddev/database/AutoConfigureDataSourceListener.java
+++ src/main/java/de/php_perfect/intellij/ddev/database/AutoConfigureDataSourceListener.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public final class AutoConfigureDataSourceListener implements DatabaseInfoChangedListener {
-    private static final @NotNull String HOST = "127.0.0.1";
+    private static final @NotNull String HOST = "192.168.0.168";
     private final @NotNull Project project;
 
 
